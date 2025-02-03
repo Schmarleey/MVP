c16dc3cc75bcaf3d4fca5bf82b28831123a609d0
Schmarley <marlon.g.becker@gmail.com>
2025-02-03 15:56:37 +0100

    Initial Commit

---
+++ MVP/MVPApp.swift
@@ -0,0 +1,17 @@
+//
+//  MVPApp.swift
+//  MVP
+//
+//  Created by Marlon Becker on 03.02.25.
+//
+
+import SwiftUI
+
+@main
+struct MVPApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MVPTests/MVPTests.swift
@@ -0,0 +1,17 @@
+//
+//  MVPTests.swift
+//  MVPTests
+//
+//  Created by Marlon Becker on 03.02.25.
+//
+
+import Testing
+@testable import MVP
+
+struct MVPTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
